22bbd1d44ce8d21c41a5b3820f471e2ee75e15ff
prashanthdsouza <79136584+prashanthdsouza@users.noreply.github.com>
2022-09-29 16:31:42 +0530

    343

--- meet-up/pages/api/new-meetup.js
+++ meet-up/pages/api/new-meetup.js
@@ -1,14 +1,25 @@
+import { MongoClient } from "mongodb";
+
 // api/new-meetup
 // POST/api/new-meetup
 
-function handler(req, res) {
-    if (req.method === 'POST') {
-        const data = req.body; 
+async function handler(req, res) {
+  if (req.method === "POST") {
+    const data = req.body;
 
-        const { title, image, address, description } = data ;
+    const client = await MongoClient.connect(
+      "mongodb+srv://admin:V@6s344rje3#n$K@cluster0.dqfrpf2.mongodb.net/meetups?retryWrites=true&w=majority"
+    );
+    const db = client.db();
 
-        
-    }
+    const meetupsCollection = db.collection("meetups");
+    const result = await meetupsCollection.insertOne({ data });
+    //
+
+    client.close();
+
+    res.status(201).json({ message: "Meetup inserted" });
+  }
 }
 
 export default handler;
