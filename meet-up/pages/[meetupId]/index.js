72dc48138417e9c0ad6e953bccfd2f29aacb4aad
prashanthdsouza <79136584+prashanthdsouza@users.noreply.github.com>
2022-09-29 14:03:45 +0530

    340

--- meet-up/pages/[meetupId]/index.js
+++ meet-up/pages/[meetupId]/index.js
@@ -1,4 +1,3 @@
-import { Fragment } from "react";
 import MeetupDetails from "../../components/meetups/MeetupDetails";
 
 function MeetupDetails() {
@@ -10,6 +9,27 @@ function MeetupDetails() {
       description="this is a first meetup"
     />
   );
+}
+
+export async function getStaticProps(context) {
+  //
+
+  const meetupId = context.params.meetupId;
+
+
+
+  return {
+    props: {
+      meetupData: {
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/1280px-Stadtbild_M%C3%BCnchen.jpg",
+        id: meetupId,
+        title: "First meetup",
+        address: "some street, some city",
+        description: "this is a first meetup",
+      },
+    },
+  };
 }
 
 export default MeetupDetails;
